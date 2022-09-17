6c2f871cc532647eafca2bf68ef93b41ff6f8189
ROCKSandCLOUDS <70162671+rocksandclouds@users.noreply.github.com>
2022-09-17 14:39:32 +0200

    Update CFX_temp.cpp

--- CFX_temp.cpp
+++ CFX_temp.cpp
@@ -10,11 +10,11 @@ const char* ssid = "SSID"; // Write here your router's username
 const char* password = "PASSWORD"; // Write here your router's password
 
 // MQTT Broker
-const char *mqtt_broker = "192.168.178.30";
-const char *topic1 = "esp8266/raw";
-const char *topic2 = "esp8266/average";
-const char *topic3 = "esp8266/temp";
+const char *mqtt_broker = "192.168.178.20";
 const int mqtt_port = 1883;
+const char *topic_verbose_raw = "esp8266/raw";
+const char *topic_verbose_average = "esp8266/average";
+const char *topic_write_value = "W/VRM-ID/temperature/3/Temperature";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -31,8 +31,42 @@ void setup() {
       Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
+  }
 
-  //connecting to a mqtt broker
+void loop() {
+  client.loop();
+
+  // read analog value from defined sensor pin;
+  int readingSensor = analogRead(sensorPin);
+
+  // calculate the average from 10 readings and a 1000ms delay between;
+  int readingAverage = 0;
+   for (int i=0; i < 30; i++) {
+   readingAverage = readingAverage + analogRead(A0);
+   delay(2000);
+   }
+   readingAverage = readingAverage/30;
+
+  // transform the calculated avarage into the temperature, based on a linear function;
+  calcTemperature = ((readingAverage-422.25)/14.5);
+
+// send messages to MQTT;
+
+/*
+  char msg[30];
+  char val[10];
+  dtostrf(readingSensor, 8, 5, val);
+  sprintf(msg, "Raw: %s", val);
+  client.publish(topic_verbose_raw, msg);
+
+  char msgAverage[30];
+  char valAverage[10];
+  dtostrf(readingAverage, 8, 5, valAverage);
+  sprintf(msgAverage, "Average: %s", valAverage);
+  client.publish(topic_verbose_average, msgAverage);
+*/
+
+  //connecting to the mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   while (!client.connected()) {
       String client_id = "esp8266-client-";
@@ -46,53 +80,12 @@ void setup() {
           delay(2000);
       }
   }
-  }
-
-void loop() {
-  client.loop();
-
-  // read analog value from defined sensor pin;
-  int readingSensor = analogRead(sensorPin);
-
-  // calculate the average from 10 readings and a 300ms delay between;
-  int readingAverage = 0;
-   for (int i=0; i < 10; i++) {
-   readingAverage = readingAverage + analogRead(A0);
-   Serial.println(readingAverage);
-   delay(300);
-   }
-   readingAverage = readingAverage/10;
-  Serial.println();
-  Serial.println("-----------------------");
-
-  // transform the calculated avarage into the temperature, based on a linear function;
-  calcTemperature = ((readingAverage-422.25)/14.5);
-  Serial.println(calcTemperature);
-  Serial.println();
-  Serial.println("-----------------------");
-
-  // send messages to MQTT;
-  char msg[30];
-  char val[10];
-  dtostrf(readingSensor, 8, 5, val);
-  sprintf(msg, "Raw: %s", val);
-  client.publish(topic1, msg);
-
-  char msgAverage[30];
-  char valAverage[10];
-  dtostrf(readingAverage, 8, 5, valAverage);
-  sprintf(msgAverage, "Durchschnitt: %s", valAverage);
-  client.publish(topic2, msgAverage);
 
   char msgTemperature[30];
   char valTemperature[10];
   dtostrf(calcTemperature, 8, 1, valTemperature);
-  sprintf(msgTemperature, "Temperature: %s", valTemperature);
-  client.publish(topic3, msgTemperature);
-  Serial.println();
-  Serial.println("----------!!!!!!!!!!!!!!----------");
-  Serial.println("FINISHED!");
-  Serial.println("----------!!!!!!!!!!!!!!----------");
-  Serial.println();
-  delay(5000);
+  sprintf(msgTemperature, "{\"value\": %s }", valTemperature);
+  client.publish(topic_write_value, msgTemperature);
+  Serial.print("Temperature: ");
+  Serial.println(calcTemperature);
 }
